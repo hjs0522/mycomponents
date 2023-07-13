4f46e8e7c609ea8f192a517caf39e886182ed4e4
hjs0522 <91768822+hjs0522@users.noreply.github.com>
2023-07-13 22:52:08 +0900

    using useMutation add PostData.tsx

--- src/fetch/api.tsx
+++ src/fetch/api.tsx
@@ -1,7 +1,21 @@
 import { PersonProps } from "../interface";
 
 export const getData = async ():Promise<PersonProps[]>=>{
-    const res = await fetch(`http://localhost:3000/api/new_data.json`);
+    const res = await fetch(`http://localhost:3000/api/data.json`);
     const data = await res.json()
     return data
+}
+
+/* 클라이언트 사이드 자바스크립트는 로컬파일에 대한 변경 권한이 없다. 그렇기에 실제로 사용할 때는
+http://localhost:3000/api/data.json이 아닌 서버의 실제 주소를 입력하여 사용해야 한다
+*/
+export const postData = async (data: PersonProps): Promise<Response> => {
+    const response = await fetch('http://localhost:3000/api/data.json', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    });
+    return response;
 }
